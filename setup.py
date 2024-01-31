57e451484e248f200d835f6f7e8a6cde3065f7b2
gabriel.athenes <gabriel.athenes@polytechnique.edu>
2024-01-31 22:31:23 +0100

    [feat] Paired option feature

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "scipy>1.11,<2",
     ],
     name="hilary",
-    version="1.0.22",
+    version="1.1.0",
     url="https://github.com/statbiophys/HILARy/",
     author="Natanael Spisak, Gabriel Athènes",
     author_email="natanael.spisak@gmail.com, gabriel.athenes@polytechnique.edu",
