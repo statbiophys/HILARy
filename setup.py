4e483e20bd1dc5201030cdd7e371e427b6c064bf
gabriel.athenes <gabrielathenes@gmail.com>
2024-04-05 18:12:33 +0200

    [~] Updating version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "scipy>1.11,<2",
     ],
     name="hilary",
-    version="1.2.0",
+    version="1.2.1",
     url="https://github.com/statbiophys/HILARy/",
     author="Gabriel Athènes,Natanael Spisak",
     author_email="gabriel.athenes@polytechnique.edu,natanael.spisak@gmail.com",
