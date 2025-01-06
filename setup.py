c0df4ff2eccf2712a62eb0d5fabc664c25c1a93b
g.athenes@saber-bio.com <g.athenes@saber-bio.com>
2025-01-06 14:26:18 +0100

    [~] Python 3.10

--- setup.py
+++ setup.py
@@ -33,6 +33,6 @@ setup(
         "Intended Audience :: Developers",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
