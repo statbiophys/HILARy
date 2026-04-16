f88de038e85d389d15b10087e85218ab3215f5c1
Thomas Dupic <32767097+Thopic@users.noreply.github.com>
2026-04-16 23:06:37 +0200

    Update apriori.py

--- hilary/apriori.py
+++ hilary/apriori.py
@@ -192,7 +192,7 @@ class Apriori:
         # add cdr3_length_value to classes for computation
         if self.paired:
             self.classes["cdr3_length_value"] = self.classes.cdr3_length.apply(
-                lambda x: int(x.split(",")[0]) + int(x.split(",")[1])
+                lambda x: int(float(x.split(",")[0])) + int(float(x.split(",")[1]))
             )
         else:
             self.classes["cdr3_length_value"] = self.classes.cdr3_length.astype(int)
